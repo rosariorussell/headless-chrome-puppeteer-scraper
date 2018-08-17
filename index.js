409cae7dda1681cae0576390a18afb19e5698ee2
Russell Rosario <rosariorussell@gmail.com>
2018-08-17 06:53:09 -0400

    added more features

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer')
 
-(async () => {
-  const browser = await puppeteer.launch({ headless: false })
+const listOfPages = [
+  'https://www.indeed.com/jobs?q=javascript+remote&l=United+States',
+  'https://www.indeed.com/jobs?q=javascript+remote&l=United+States&start=10',
+  'https://www.indeed.com/jobs?q=javascript+remote&l=United+States&start=20',
+  'https://www.indeed.com/jobs?q=javascript+remote&l=United+States&start=30',
+  'https://www.indeed.com/jobs?q=javascript+remote&l=United+States&start=40',
+  'https://www.indeed.com/jobs?q=javascript+remote&l=United+States&start=50',
+  'https://www.indeed.com/jobs?q=javascript+remote&l=United+States&start=60',
+  'https://www.indeed.com/jobs?q=javascript+remote&l=United+States&start=70',
+  'https://www.indeed.com/jobs?q=javascript+remote&l=United+States&start=80',
+  'https://www.indeed.com/jobs?q=javascript+remote&l=United+States&start=90'
+]
+
+const cssSelector = '#resultsCol .row[data-tn-component="organicJob"]'
+
+const scrape = async (pageToScrape) => {
+  const browser = await puppeteer.launch({ devtools: false, headless: false, args: ['--start-fullscreen'] })
   const page = await browser.newPage()
+  await page.setViewport({ width: 2080, height: 1200 })
 
   // CONFIGURE LOGIN IF NECESSARY
   // const { username, password } = require('./creds')
@@ -13,20 +29,30 @@ const puppeteer = require('puppeteer');
   // await page.click('#btn-primary')
 
   // START SCRAPING
-  const pageToScrape = 'https://www.indeed.com/jobs?q=javascript+remote&l=United+States'
-  const cssSelector = '#resultsCol .row[data-tn-component="organicJob"]'
+
   await page.goto(pageToScrape, { 'waitUntil': 'networkidle0' })
 
   // FORMAT RESULTS
   let results = await page.$$eval(cssSelector, el => {
     return el.map((e) => {
       return {
+        // url: e.children[0].innerHTML,
         position: e.children[0].innerText,
         company: e.children[1].innerText,
         location: e.children[2].innerText
       }
     })
   })
+
+  // EXTRACT LINKS FROM ELEMENTS
+  // const urls = await page.evaluate(() => {
+  //   const links = Array.from(document.querySelectorAll('.pagination > a'))
+  //   return links.map(link => link.href)
+  // })
+
+  // HANDLE BUTTON PAGINATION
+  // await page.click('.pagination > a:nth-child(6) > span')
+  // await page.waitFor(5000)
 
   // PERFORM REGEX IF NECESSARY
   // const urlRegex = /https:\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?/
@@ -48,11 +74,17 @@ const puppeteer = require('puppeteer');
   // TAKE A SCREENSHOT
   // await page.screenshot({ path: 'example.png' })
 
-  // PAGINATION
-  // await page.click('.next')
-  // await page.waitFor(5000)
-
-  console.log(results)
-
   await browser.close()
+  return results
+}
+
+(async function runMultiPageScrape () {
+  let fullResults = []
+  let start = new Date()
+  for (let page of listOfPages) {
+    const result = await scrape(page)
+    fullResults.push(...result)
+  }
+  let end = new Date()
+  console.log(fullResults.length, 'results collected in', (end - start) / 1000, 'seconds')
 })()
